de142bd3f648bcdb3f2ebcd548bb522c86a72211
Dev App <88029262+DevAppHub@users.noreply.github.com>
2022-01-14 14:55:28 +0530

    Update TestController.java

--- src/main/java/com/cts/estock/api/TestController.java
+++ src/main/java/com/cts/estock/api/TestController.java
@@ -1,6 +1,7 @@
 package com.cts.estock.api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/test")
